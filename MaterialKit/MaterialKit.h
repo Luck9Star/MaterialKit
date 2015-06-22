44bd68ab299cf4d2cefb83c46c6ba4303833997b
LuckyStar <hadyenvip@gmail.com>
2015-06-22 18:36:44 +0800

    Swift 2.0 初步去bug

---
+++ MaterialKit/MaterialKit.h
@@ -0,0 +1,19 @@
+//
+//  MaterialKit.h
+//  MaterialKit
+//
+//  Created by Ethan Yang on 15/6/22.
+//  Copyright © 2015年 Ethan Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MaterialKit.
+FOUNDATION_EXPORT double MaterialKitVersionNumber;
+
+//! Project version string for MaterialKit.
+FOUNDATION_EXPORT const unsigned char MaterialKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaterialKit/PublicHeader.h>
+
+
